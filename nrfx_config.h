1e8103aeb5778512d8fccbfca0117681a09cbad5
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2019-11-06 17:52:00 +0100

    nrfx: Update to version 2.0.0
    Update nrfx to the recently released version. See
    https://github.com/NordicSemiconductor/nrfx/blob/v2.0.0/CHANGELOG.md
    for a list of changes that this version introduces.
    
    Origin: nrfx
    License: BSD 3-Clause
    URL: https://github.com/NordicSemiconductor/nrfx/tree/v2.0.0
    commit: 7a4c9d946cf1801771fc180acdbf7b878f270093
    Purpose: Provide peripheral drivers for Nordic SoCs
    Maintained-by: External
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- nrfx_config.h
+++ nrfx_config.h
@@ -354,8 +354,14 @@
     #include <nrfx_config_nrf52811.h>
 #elif defined(NRF52832_XXAA) || defined (NRF52832_XXAB)
     #include <nrfx_config_nrf52832.h>
+#elif defined(NRF52833_XXAA)
+    #include <nrfx_config_nrf52833.h>
 #elif defined(NRF52840_XXAA)
     #include <nrfx_config_nrf52840.h>
+#elif defined(NRF5340_XXAA_APPLICATION)
+    #include <nrfx_config_nrf5340_application.h>
+#elif defined(NRF5340_XXAA_NETWORK)
+    #include <nrfx_config_nrf5340_network.h>
 #elif defined(NRF9160_XXAA)
     #include <nrfx_config_nrf9160.h>
 #else
