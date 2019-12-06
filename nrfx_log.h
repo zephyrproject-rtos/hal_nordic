b0709c2f08d7ecd3ab0bca30f012b010f56d1ab9
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2019-12-06 12:08:26 +0100

    nrfx_log: Interface nrfx logs with Zephyr logging subsystem
    Provide an adaptation layer that will use the Zephyr logging subsystem
    for handling log messages from nrfx drivers.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- nrfx_log.h
+++ nrfx_log.h
@@ -36,6 +36,35 @@
 extern "C" {
 #endif
 
+#define NRFX_MODULE_PREFIX  _CONCAT(NRFX_, NRFX_LOG_MODULE)
+/*
+ * The following macros from nrfx_config control the log messages coming from
+ * a given module:
+ * - NRFX_<module>_CONFIG_LOG_ENABLED enables the messages (when set to 1)
+ * - NRFX_<module>_CONFIG_LOG_LEVEL specifies the severity level of the messages
+ *   that are to be output.
+ */
+#if !IS_ENABLED(_CONCAT(NRFX_MODULE_PREFIX, _CONFIG_LOG_ENABLED))
+#define NRFX_MODULE_CONFIG_LOG_LEVEL 0
+#else
+#define NRFX_MODULE_CONFIG_LOG_LEVEL \
+	_CONCAT(NRFX_MODULE_PREFIX, _CONFIG_LOG_LEVEL)
+#endif
+
+#if	NRFX_MODULE_CONFIG_LOG_LEVEL == 0
+#define NRFX_MODULE_LOG_LEVEL		LOG_LEVEL_NONE
+#elif	NRFX_MODULE_CONFIG_LOG_LEVEL == 1
+#define NRFX_MODULE_LOG_LEVEL		LOG_LEVEL_ERR
+#elif	NRFX_MODULE_CONFIG_LOG_LEVEL == 2
+#define NRFX_MODULE_LOG_LEVEL		LOG_LEVEL_WRN
+#elif	NRFX_MODULE_CONFIG_LOG_LEVEL == 3
+#define NRFX_MODULE_LOG_LEVEL		LOG_LEVEL_INF
+#elif	NRFX_MODULE_CONFIG_LOG_LEVEL == 4
+#define NRFX_MODULE_LOG_LEVEL		LOG_LEVEL_DBG
+#endif
+#include <logging/log.h>
+LOG_MODULE_REGISTER(NRFX_MODULE_PREFIX, NRFX_MODULE_LOG_LEVEL);
+
 /**
  * @defgroup nrfx_log nrfx_log.h
  * @{
@@ -48,35 +77,34 @@ extern "C" {
 /**
  * @brief Macro for logging a message with the severity level ERROR.
  *
- * @param format printf-style format string, optionally followed by arguments
- *               to be formatted and inserted in the resulting string.
+ * @param ... printf-style format string, optionally followed by arguments
+ *            to be formatted and inserted in the resulting string.
  */
-#define NRFX_LOG_ERROR(format, ...)
+#define NRFX_LOG_ERROR(...)  LOG_ERR(__VA_ARGS__)
 
 /**
  * @brief Macro for logging a message with the severity level WARNING.
  *
- * @param format printf-style format string, optionally followed by arguments
- *               to be formatted and inserted in the resulting string.
+ * @param ... printf-style format string, optionally followed by arguments
+ *            to be formatted and inserted in the resulting string.
  */
-#define NRFX_LOG_WARNING(format, ...)
+#define NRFX_LOG_WARNING(...)  LOG_WRN(__VA_ARGS__)
 
 /**
  * @brief Macro for logging a message with the severity level INFO.
  *
- * @param format printf-style format string, optionally followed by arguments
- *               to be formatted and inserted in the resulting string.
+ * @param ... printf-style format string, optionally followed by arguments
+ *            to be formatted and inserted in the resulting string.
  */
-#define NRFX_LOG_INFO(format, ...)
+#define NRFX_LOG_INFO(...)  LOG_INF(__VA_ARGS__)
 
 /**
  * @brief Macro for logging a message with the severity level DEBUG.
  *
- * @param format printf-style format string, optionally followed by arguments
- *               to be formatted and inserted in the resulting string.
+ * @param ... printf-style format string, optionally followed by arguments
+ *            to be formatted and inserted in the resulting string.
  */
-#define NRFX_LOG_DEBUG(format, ...)
-
+#define NRFX_LOG_DEBUG(...)  LOG_DBG(__VA_ARGS__)
 
 /**
  * @brief Macro for logging a memory dump with the severity level ERROR.
@@ -84,7 +112,8 @@ extern "C" {
  * @param[in] p_memory  Pointer to the memory region to be dumped.
  * @param[in] length    Length of the memory region in bytes.
  */
-#define NRFX_LOG_HEXDUMP_ERROR(p_memory, length)
+#define NRFX_LOG_HEXDUMP_ERROR(p_memory, length) \
+	LOG_HEXDUMP_ERR(p_memory, length, "")
 
 /**
  * @brief Macro for logging a memory dump with the severity level WARNING.
@@ -92,7 +121,8 @@ extern "C" {
  * @param[in] p_memory  Pointer to the memory region to be dumped.
  * @param[in] length    Length of the memory region in bytes.
  */
-#define NRFX_LOG_HEXDUMP_WARNING(p_memory, length)
+#define NRFX_LOG_HEXDUMP_WARNING(p_memory, length) \
+	LOG_HEXDUMP_WRN(p_memory, length, "")
 
 /**
  * @brief Macro for logging a memory dump with the severity level INFO.
@@ -100,7 +130,8 @@ extern "C" {
  * @param[in] p_memory  Pointer to the memory region to be dumped.
  * @param[in] length    Length of the memory region in bytes.
  */
-#define NRFX_LOG_HEXDUMP_INFO(p_memory, length)
+#define NRFX_LOG_HEXDUMP_INFO(p_memory, length) \
+	LOG_HEXDUMP_INF(p_memory, length, "")
 
 /**
  * @brief Macro for logging a memory dump with the severity level DEBUG.
@@ -108,8 +139,8 @@ extern "C" {
  * @param[in] p_memory  Pointer to the memory region to be dumped.
  * @param[in] length    Length of the memory region in bytes.
  */
-#define NRFX_LOG_HEXDUMP_DEBUG(p_memory, length)
-
+#define NRFX_LOG_HEXDUMP_DEBUG(p_memory, length) \
+	LOG_HEXDUMP_DBG(p_memory, length, "")
 
 /**
  * @brief Macro for getting the textual representation of a given error code.
@@ -118,7 +149,8 @@ extern "C" {
  *
  * @return String containing the textual representation of the error code.
  */
-#define NRFX_LOG_ERROR_STRING_GET(error_code)
+#define NRFX_LOG_ERROR_STRING_GET(error_code)  nrfx_error_string_get(error_code)
+extern char const *nrfx_error_string_get(nrfx_err_t code);
 
 /** @} */
 
