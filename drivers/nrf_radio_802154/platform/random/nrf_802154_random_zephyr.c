5be3200c99186f94eb368cc4c266b78abfe34265
Robert Lubos <robert.lubos@nordicsemi.no>
2020-03-16 12:25:13 +0100

    drivers: nrf_radio_802154: Add PRNG implementation
    So far radio driver used system function `sys_rand32_get`. This
    function, in default configuration for nRF is based on
    `entropy_get_entropy` function which is not callable from an ISR.
    
    As with CSMA/CA introducion `nrf_802154_random_get` can be called
    from an ISR context, the default generator can no longer be used.
    Add xoroshiro128+ PRNG implementation and use it instead.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- drivers/nrf_radio_802154/platform/random/nrf_802154_random_zephyr.c
+++ drivers/nrf_radio_802154/platform/random/nrf_802154_random_zephyr.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019, Nordic Semiconductor ASA
+/* Copyright (c) 2019 - 2020, Nordic Semiconductor ASA
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,23 +28,58 @@
  *
  */
 
+/*
+ * Based on code written in 2016-2018 by David Blackman and Sebastiano Vigna
+ * (vigna@acm.org)
+ *
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * See <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 /**
  * @file
  *   This file implements the pseudo-random number generator abstraction layer.
  *
- * This pseudo-random number abstraction layer uses standard library rand() function.
+ * This pseudo-random number abstraction layer implements xoroshiro128+ algorithm.
  *
  */
 
 #include "nrf_802154_random.h"
 
-#include <zephyr.h>
+#include <drivers/entropy.h>
 
-#include "nrf.h"
+static uint64_t s[2];
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t next(void)
+{
+    const uint64_t s0 = s[0];
+    uint64_t s1 = s[1];
+    const uint64_t result = s0 + s1;
+
+    s1 ^= s0;
+    s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+    s[1] = rotl(s1, 37); // c
+
+    return result;
+}
 
 void nrf_802154_random_init(void)
 {
-    // Intentionally empty
+    struct device *dev;
+    int err;
+
+    dev = device_get_binding(CONFIG_ENTROPY_NAME);
+    __ASSERT_NO_MSG(dev != NULL);
+
+    err = entropy_get_entropy(dev, (uint8_t *)&s, sizeof(s));
+    __ASSERT_NO_MSG(err == 0);
 }
 
 void nrf_802154_random_deinit(void)
@@ -54,5 +89,5 @@ void nrf_802154_random_deinit(void)
 
 uint32_t nrf_802154_random_get(void)
 {
-    return sys_rand32_get();
+    return (uint32_t)next();
 }
