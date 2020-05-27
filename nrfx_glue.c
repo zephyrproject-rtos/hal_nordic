00580f43b9d48f2079d611ce12f0fbc78d3040f1
Kumar Gala <kumar.gala@linaro.org>
2020-05-27 12:14:31 -0500

    nrfx_glue: Convert from Zephyr to C99 types
    Convert glue code to use C99 types as we intend to deprecate the zephyr
    specific integer types.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- nrfx_glue.c
+++ nrfx_glue.c
@@ -12,7 +12,7 @@ void nrfx_isr(void *irq_handler)
 	((nrfx_irq_handler_t)irq_handler)();
 }
 
-void nrfx_busy_wait(u32_t usec_to_wait)
+void nrfx_busy_wait(uint32_t usec_to_wait)
 {
 	k_busy_wait(usec_to_wait);
 }
