7538628119a5b307a1aef53e52b8d216419e7ad7
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2020-08-20 14:18:59 +0200

    nrfx: Update to version 2.3.0
    Update nrfx to the recently released version. See
    https://github.com/NordicSemiconductor/nrfx/blob/v2.3.0/CHANGELOG.md
    for a list of changes that this version introduces.
    
    Origin: nrfx
    License: BSD 3-Clause
    URL: https://github.com/NordicSemiconductor/nrfx/tree/v2.3.0
    commit: 6c31cb7d0e4038afa1efc9bfed40206adfd7b83e
    Purpose: Provide peripheral drivers for Nordic SoCs
    Maintained-by: External
    
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>

--- nrfx/hal/nrf_common.h
+++ nrfx/hal/nrf_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2020, Nordic Semiconductor ASA
+ * Copyright (c) 2020, Nordic Semiconductor ASA
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,55 +29,32 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef NRFX_USBD_ERRATA_H__
-#define NRFX_USBD_ERRATA_H__
+#ifndef NRF_COMMON_H__
+#define NRF_COMMON_H__
 
-#include <nrfx.h>
-#include <nrf_erratas.h>
-
-#ifndef NRFX_USBD_ERRATA_ENABLE
-/**
- * @brief The constant that informs if errata should be enabled at all.
- *
- * If this constant is set to 0, all the Errata bug fixes will be automatically disabled.
- */
-#define NRFX_USBD_ERRATA_ENABLE 1
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-/* Errata: ISO double buffering not functional. **/
-static inline bool nrfx_usbd_errata_166(void)
+#ifndef NRFX_EVENT_READBACK_ENABLED
+#define NRFX_EVENT_READBACK_ENABLED 1
+#endif
+
+#ifndef NRF_DECLARE_ONLY
+
+NRF_STATIC_INLINE void nrf_event_readback(void * p_event_reg)
 {
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_166();
+#if NRFX_CHECK(NRFX_EVENT_READBACK_ENABLED) && !defined(NRF51)
+    (void)*((volatile uint32_t *)(p_event_reg));
+#else
+    (void)p_event_reg;
+#endif
 }
 
-/* Errata: USBD might not reach its active state. **/
-static inline bool nrfx_usbd_errata_171(void)
-{
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_171();
-}
+#endif // NRF_DECLARE_ONLY
 
-/* Errata: USB cannot be enabled. **/
-static inline bool nrfx_usbd_errata_187(void)
-{
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_187();
+#ifdef __cplusplus
 }
+#endif
 
-/* Errata: USBD cannot receive tasks during DMA. **/
-static inline bool nrfx_usbd_errata_199(void)
-{
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_199();
-}
-
-/* Errata: Device remains in SUSPEND too long. */
-static inline bool nrfx_usbd_errata_211(void)
-{
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_211();
-}
-
-/* Errata: Unexpected behavior after reset. **/
-static inline bool nrfx_usbd_errata_223(void)
-{
-    return NRFX_USBD_ERRATA_ENABLE && nrf52_errata_223();
-}
-
-#endif // NRFX_USBD_ERRATA_H__
+#endif // NRF_COMMON_H__
