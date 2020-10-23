23647c00af54d233d69dbd1efe316213e53506b5
Czeslaw Makarski <Czeslaw.Makarski@nordicsemi.no>
2020-10-23 12:46:10 +0200

    drivers: nrf_radio_802154: Introduce v1.9 of the Radio Driver
    This commit introduces the Radio Driver v1.9
    
    Signed-off-by: Czeslaw Makarski <Czeslaw.Makarski@nordicsemi.no>

---
+++ drivers/nrf_radio_802154/src/platform/random/nrf_802154_random_zephyr.c
@@ -0,0 +1,94 @@
+/* Copyright (c) 2019 - 2020, Nordic Semiconductor ASA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, this
+ *      list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *
+ *   3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Based on code written in 2016-2018 by David Blackman and Sebastiano Vigna
+ * (vigna@acm.org)
+ *
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * See <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+/**
+ * @file
+ *   This file implements the pseudo-random number generator abstraction layer.
+ *
+ * This pseudo-random number abstraction layer implements xoroshiro128+ algorithm.
+ *
+ */
+
+#include "nrf_802154_random.h"
+
+#include <drivers/entropy.h>
+
+static uint64_t s[2];
+
+static inline uint64_t rotl(const uint64_t x, int k)
+{
+    return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t next(void)
+{
+    const uint64_t s0     = s[0];
+    uint64_t       s1     = s[1];
+    const uint64_t result = s0 + s1;
+
+    s1  ^= s0;
+    s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+    s[1] = rotl(s1, 37);                   // c
+
+    return result;
+}
+
+void nrf_802154_random_init(void)
+{
+    const struct device * dev;
+    int                   err;
+
+    dev = device_get_binding(DT_CHOSEN_ZEPHYR_ENTROPY_LABEL);
+    __ASSERT_NO_MSG(dev != NULL);
+
+    err = entropy_get_entropy(dev, (uint8_t *)&s, sizeof(s));
+    __ASSERT_NO_MSG(err == 0);
+}
+
+void nrf_802154_random_deinit(void)
+{
+    // Intentionally empty
+}
+
+uint32_t nrf_802154_random_get(void)
+{
+    return (uint32_t)next();
+}
