9b3a4bc5add0cc7c39b127ccbb63e193e0bc65f9
Pawel Kwiek <pawel.kwiek@nordicsemi.no>
2021-01-12 17:46:55 +0100

    drivers: Add CMake file list variables for IEEE 802.15.4
    This commit introduces file list variables providing an abstraction for
    files required for IEEE 802.15.4 driver to be built.
    
    Signed-off-by: Pawel Kwiek <pawel.kwiek@nordicsemi.no>

---
+++ drivers/nrf_802154_sl_opensource/nrf_802154_sl_files.cmake
@@ -0,0 +1,28 @@
+set(NRF_802154_SL_OPENSOURCE_SOURCES
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_ant_div.c
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_coex.c
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_fem.c
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_log.c
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_rsch.c
+  ${NRF_802154_SL_ROOT}/src/nrf_802154_sl_timer.c
+  ${NRF_802154_SL_ROOT}/src/platform/clock/nrf_802154_clock_zephyr.c
+  ${NRF_802154_SL_ROOT}/src/platform/irq/nrf_802154_irq_zephyr.c
+)
+
+set(NRF_802154_SL_OPENSOURCE_INCLUDE_DIRS
+  ${NRF_802154_SL_ROOT}/include
+  ${NRF_802154_SL_ROOT}/src
+)
+
+set(NRF_802154_SL_SOURCES
+  ${NRF_802154_SL_ROOT}/src/platform/gpiote/nrf_802154_gpiote_zephyr.c
+  ${NRF_802154_SL_ROOT}/src/platform/gpiote/nrf_802154_gpiote_crit_sect.c
+  ${NRF_802154_SL_ROOT}/src/platform/hp_timer/nrf_802154_hp_timer.c
+  ${NRF_802154_SL_ROOT}/src/platform/lp_timer/nrf_802154_lp_timer.c
+  ${NRF_802154_SL_ROOT}/src/platform/clock/nrf_802154_clock_zephyr.c
+  ${NRF_802154_SL_ROOT}/src/platform/irq/nrf_802154_irq_zephyr.c
+)
+
+set(NRF_802154_SL_INCLUDE_DIRS
+  ${NRF_802154_SL_ROOT}/src
+)
