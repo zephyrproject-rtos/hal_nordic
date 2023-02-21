209ec76ed74b161d7f19ab855bd26c8c1b3d07d7
Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>
2023-02-21 14:20:32 +0100

    drivers: nrf_802154: Update the IEEE 802.15.4 component
    This commit updates the nRF 802.15.4 radio driver to feature the latest
    changes.
    
    sdk-nrf-802154 commit: 9470a97f6e07d2f3ae09db2e6cefa5edf0b2b9ae
    
    Signed-off-by: Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>

--- drivers/nrf_802154/driver/src/nrf_802154_trx.c
+++ drivers/nrf_802154/driver/src/nrf_802154_trx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - 2022, Nordic Semiconductor ASA
+ * Copyright (c) 2019 - 2023, Nordic Semiconductor ASA
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -259,6 +259,37 @@ void nrf_timer_init(void)
 #endif
 }
 
+#if defined(NRF53_SERIES)
+/** Implement the YOPAN-158 workaround. */
+static void yopan_158_workaround(void)
+{
+#define RADIO_ADDRESS_MASK        0xFFFFF000UL
+#define FICR_TRIM_REGISTERS_COUNT 32UL
+    /* This is a workaround for an issue reported in YOPAN-158.
+     *
+     * After RADIO peripheral reset with RADIO.POWER register the trim-values, loaded from FICR at
+     * network core boot time by MDK, are lost. The trim-values are not preserved and re-applied by
+     * hardware.
+     *
+     * Only selected trim-values are restored, those that apply to RADIO peripheral. The check
+     * is done based on destination address.
+     */
+
+    /* Copy all the trimming values from FICR into the target addresses. Trim until one ADDR
+       is not initialized. */
+    for (uint32_t index = 0; index < FICR_TRIM_REGISTERS_COUNT; index++)
+    {
+        if (((volatile uint32_t *)((volatile uintptr_t)NRF_FICR_NS->TRIMCNF[index].ADDR &
+                                   (uintptr_t)RADIO_ADDRESS_MASK) == (uint32_t *)NRF_RADIO))
+        {
+            *((volatile uint32_t *)NRF_FICR_NS->TRIMCNF[index].ADDR) =
+                NRF_FICR_NS->TRIMCNF[index].DATA;
+        }
+    }
+}
+
+#endif /* NRF53_SERIES */
+
 /** Reset radio peripheral. */
 static void nrf_radio_reset(void)
 {
@@ -266,6 +297,10 @@ static void nrf_radio_reset(void)
 
     nrf_radio_power_set(NRF_RADIO, false);
     nrf_radio_power_set(NRF_RADIO, true);
+
+#if defined(NRF53_SERIES)
+    yopan_158_workaround();
+#endif /* NRF53_SERIES */
 
     nrf_802154_log_global_event(NRF_802154_LOG_VERBOSITY_LOW,
                                 NRF_802154_LOG_GLOBAL_EVENT_ID_RADIO_RESET,
@@ -334,6 +369,8 @@ static void fem_for_lna_reset(void)
      */
 }
 
+#if NRF_802154_CARRIER_FUNCTIONS_ENABLED
+
 /** Configure FEM to set PA at appropriate time.
  *
  * @note This function must be called before ramp up PPIs are configured.
@@ -360,6 +397,8 @@ static void fem_for_pa_reset(void)
     nrf_802154_trx_ppi_for_fem_clear();
     mpsl_fem_deactivate_now(MPSL_FEM_PA);
 }
+
+#endif // NRF_802154_CARRIER_FUNCTIONS_ENABLED
 
 /** Configure FEM for TX procedure.
  *
@@ -806,10 +845,10 @@ void nrf_802154_trx_antenna_update(void)
 #if NRF_802154_CARRIER_FUNCTIONS_ENABLED
         case TRX_STATE_CONTINUOUS_CARRIER:
         case TRX_STATE_MODULATED_CARRIER:
+#endif // NRF_802154_CARRIER_FUNCTIONS_ENABLED
             tx_antenna_update();
             break;
 
-#endif // NRF_802154_CARRIER_FUNCTIONS_ENABLED
         default:
             /* Intentionally empty */
             break;
