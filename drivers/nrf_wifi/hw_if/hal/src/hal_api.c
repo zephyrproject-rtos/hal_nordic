krish2718/sdk-nrf

drivers: wifi: Use a separate variable to track host assertion
In case host is asserting wakeup_now, don't rely on RPU PS state, as
that is derived from multiples sources wakeup_now, RPU interrupt etc.

Use a dedicated flag to track.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

drivers/wifi/nrf700x/osal/hw_if/hal/src/hal_api.c
@@ -332,6 +332,7 @@ enum nrf_wifi_status hal_rpu_ps_wake(struct nrf_wifi_hal_dev_ctx *hal_dev_ctx)
 	}
 
 	nrf_wifi_bal_rpu_ps_wake(hal_dev_ctx->bal_dev_ctx);
+	hal_dev_ctx->is_wakup_now_asserted = true;
 
 	start_time_us = nrf_wifi_osal_time_get_curr_us(hal_dev_ctx->hpriv->opriv);
 
@@ -400,6 +401,7 @@ static void hal_rpu_ps_sleep(unsigned long data)
 					&flags);
 
 	nrf_wifi_bal_rpu_ps_sleep(hal_dev_ctx->bal_dev_ctx);
+	hal_dev_ctx->is_wakup_now_asserted = false;
 
 	hal_dev_ctx->rpu_ps_state = RPU_PS_STATE_ASLEEP;
 

zephyrproject-rtos/hal_nordic

drivers/nrf_wifi/hw_if/hal/src/hal_api.c
@@ -316,6 +316,7 @@ enum nrf_wifi_status hal_rpu_ps_wake(struct nrf_wifi_hal_dev_ctx *hal_dev_ctx)
 	}
 
 	nrf_wifi_bal_rpu_ps_wake(hal_dev_ctx->bal_dev_ctx);
+	hal_dev_ctx->is_wakeup_now_asserted = true;
 
 	start_time_us = nrf_wifi_osal_time_get_curr_us();
 
@@ -377,6 +378,7 @@ static void hal_rpu_ps_sleep(unsigned long data)
 					&flags);
 
 	nrf_wifi_bal_rpu_ps_sleep(hal_dev_ctx->bal_dev_ctx);
+	hal_dev_ctx->is_wakeup_now_asserted = false;
 
 	hal_dev_ctx->rpu_ps_state = RPU_PS_STATE_ASLEEP;
 