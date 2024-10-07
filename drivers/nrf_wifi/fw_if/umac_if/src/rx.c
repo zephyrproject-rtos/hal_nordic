nrfconnect/sdk-nrf

drivers: wifi: Enable management buffers offload
With this offload, host doesn't need to manage RX buffers for management
frames, and this saves Host-RPU comms and thus giving RPU to sleep more
often and is essential to test RPU recovery.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

drivers/wifi/nrf700x/osal/fw_if/umac_if/src/rx.c
@@ -351,6 +351,9 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 #endif /* CONFIG_WIFI_MGMT_RAW_SCAN_RESULTS */
 			nrf_wifi_osal_nbuf_free(fmac_dev_ctx->fpriv->opriv,
 						nwb);
+#ifdef CONFIG_NRF_WIFI_MGMT_BUFF_OFFLOAD
+			goto out;
+#endif /* CONFIG_NRF_WIFI_MGMT_BUFF_OFFLOAD */
 		} else {
 			nrf_wifi_osal_log_err(fmac_dev_ctx->fpriv->opriv,
 					      "%s: Invalid frame type recd %d\n",

zephyrproject-rtos/hal_nordic

drivers/nrf_wifi/fw_if/umac_if/src/rx.c
@@ -349,6 +349,9 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 							config->signal);
 #endif /* CONFIG_WIFI_MGMT_RAW_SCAN_RESULTS */
 			nrf_wifi_osal_nbuf_free(nwb);
+#ifdef NRF_WIFI_MGMT_BUFF_OFFLOAD
+			goto out;
+#endif /* NRF_WIFI_MGMT_BUFF_OFFLOAD */
 		}
 #if defined(NRF70_RAW_DATA_RX) || defined(NRF70_PROMISC_DATA_RX)
 		else if (config->rx_pkt_type == NRF_WIFI_RAW_RX_PKT) {