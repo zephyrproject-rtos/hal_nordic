c29558648151965126ee79ba1fc2e0233d3dc25b
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-06-11 16:45:55 +0200

    nrfx: cracen: align to nrf54lm20a
    Support the new HW IP version included in the 54LM20 and newer devices
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- nrfx/drivers/src/nrfx_cracen.c
+++ nrfx/drivers/src/nrfx_cracen.c
@@ -42,10 +42,17 @@
 #include <soc/nrfx_coredep.h>
 
 /* TRNG HW chosen configuration options */
+#if defined(NRF54L15_XXAA) || defined(NRF54L10_XXAA) || defined(NRF54L05_XXAA)
 #define TRNG_CLK_DIV                0
+#else
+#define TRNG_CLK_DIV                1
+#endif
 #define TRNG_OFF_TIMER_VAL          0
 #define TRNG_INIT_WAIT_VAL        512
 #define TRNG_NUMBER_128BIT_BLOCKS   4
+#if NRF_CRACEN_RNG_HAS_BLENDING
+#define TRNG_BLENDING_METHOD      NRF_CRACEN_RNG_BLENDINGMETHOD_CONCATENATION
+#endif
 
 #define TRNG_CONDITIONING_KEY_SIZE 4 /* Size of the conditioning key: 4 words, 16 bytes */
 
@@ -89,7 +96,9 @@ static void trng_init(void)
     nrf_cracen_rng_control_set(NRF_CRACENCORE, &control_reset);
 
     /* Change from configuration defaults to what we prefer: */
+#if NRF_CRACEN_RNG_HAS_IDLETIMER
     nrf_cracen_rng_off_timer_set(NRF_CRACENCORE, TRNG_OFF_TIMER_VAL);
+#endif
     nrf_cracen_rng_clk_div_set(NRF_CRACENCORE, TRNG_CLK_DIV);
     nrf_cracen_rng_init_wait_val_set(NRF_CRACENCORE, TRNG_INIT_WAIT_VAL);
 
@@ -97,6 +106,9 @@ static void trng_init(void)
     static const nrf_cracen_rng_control_t control_enable = {
             .enable = true,
             .number_128_blocks = TRNG_NUMBER_128BIT_BLOCKS,
+#if NRF_CRACEN_RNG_HAS_BLENDING
+            .blending_method = TRNG_BLENDING_METHOD,
+#endif
     };
 
     nrf_cracen_rng_control_set(NRF_CRACENCORE, &control_enable);
