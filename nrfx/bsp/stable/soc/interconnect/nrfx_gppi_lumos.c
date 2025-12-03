f9005e76cab75e548b3fc6941ea07d382e84823f
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-12-03 11:27:48 +0100

    nrfx: Updates around GPPI helper
    Updated documentation in nrfx_gppi.h.
    Cleaned gppiv1_shim used on nrf54h20.
    Updated GPPI PPI implementation to allow bsim support.
    Updated migration guide.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- nrfx/bsp/stable/soc/interconnect/nrfx_gppi_lumos.c
+++ nrfx/bsp/stable/soc/interconnect/nrfx_gppi_lumos.c
@@ -111,7 +111,7 @@ static const nrfx_gppi_route_t **dppi_route_map[] = {
 	mcu_routes, rad_routes, peri_routes, lp_routes
 };
 
-uint32_t nrfx_gppi_domain_id_get(uint32_t addr)
+__WEAK uint32_t nrfx_gppi_domain_id_get(uint32_t addr)
 {
 	return ((addr >> 18) & 0x7) - 1;
 }
